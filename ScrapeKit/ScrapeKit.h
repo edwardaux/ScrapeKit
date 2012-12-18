5de47696535f4e1dded362859a25ade2dc9a687d
Craig Edwards <craig@hae.com.au>
2012-12-19 06:38:00 +1100

    Initial Commit

---
+++ ScrapeKit/ScrapeKit.h
@@ -0,0 +1,13 @@
+//
+//  ScrapeKit.h
+//  ScrapeKit
+//
+//  Created by Craig Edwards on 19/12/12.
+//  Copyright (c) 2012 BlackDog Foundry. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ScrapeKit : NSObject
+
+@end
---
+++ ScrapeKit/ScrapeKit.m
@@ -0,0 +1,13 @@
+//
+//  ScrapeKit.m
+//  ScrapeKit
+//
+//  Created by Craig Edwards on 19/12/12.
+//  Copyright (c) 2012 BlackDog Foundry. All rights reserved.
+//
+
+#import "ScrapeKit.h"
+
+@implementation ScrapeKit
+
+@end
