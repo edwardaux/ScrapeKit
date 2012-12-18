cb89af93218f19ef30daf3d4feedc0e1a4723c96
Craig Edwards <craig@hae.com.au>
2012-12-19 07:07:10 +1100

    Changed directory hierarchy and added initial rule

---
+++ ScrapeKit/Classes/rules/SKRule.m
@@ -0,0 +1,13 @@
+//
+//  SKRule.m
+//  ScrapeKit
+//
+//  Created by Craig Edwards on 19/12/12.
+//  Copyright (c) 2012 BlackDog Foundry. All rights reserved.
+//
+
+#import "SKRule.h"
+
+@implementation SKRule
+
+@end
